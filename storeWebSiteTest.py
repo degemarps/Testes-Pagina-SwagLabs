45821d320ad6cc54ad3f73e508b29a66a8cfb2a1
degemarps <degemarpereira@gmail.com>
2022-05-27 18:47:20 -0300

    Adiciona os item ao carrinho e depois verifica se foram adicionados corretamente

--- storeWebSiteTest.py
+++ storeWebSiteTest.py
@@ -27,8 +27,32 @@ try:
 except:
     driver.quit()
 
+time.sleep(2)
+
 filter_drop_down = Select(driver.find_element(By.CLASS_NAME, 'product_sort_container'))
 filter_drop_down.select_by_visible_text('Price (low to high)')
+
+sauce_labs_onesie_btn = driver.find_element(By.ID, 'add-to-cart-sauce-labs-onesie')
+sauce_labs_onesie_btn.click()
+
+t_shirt_red_btn = driver.find_element(By.ID, 'add-to-cart-test.allthethings()-t-shirt-(red)')
+t_shirt_red_btn.click()
+
+shopping_cart_btn = driver.find_element(By.CLASS_NAME, 'shopping_cart_link')
+shopping_cart_btn.click()
+
+try:
+    cart_list = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'cart_list'))
+    )
+    sauce_labs_onesie_item = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'Sauce Labs Onesie'))
+    )
+    t_shirt_red_item = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'Test.allTheThings() T-Shirt (Red)'))
+    )
+except:
+    driver.quit()
 
 time.sleep(5)
 
